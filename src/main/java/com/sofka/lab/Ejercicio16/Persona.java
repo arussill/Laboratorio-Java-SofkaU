eee5091eb8d1de523cfc53854a9d9024695811c9
Aura Russill <auracris1996@hotmail.com>
2022-04-19 21:47:59 -0500

    Ejercicio16 metodo generarDNI y Setters

--- src/main/java/com/sofka/lab/Ejercicio16/Persona.java
+++ src/main/java/com/sofka/lab/Ejercicio16/Persona.java
@@ -13,54 +13,77 @@ public class Persona {
     private final int pesoIdeal = -1;
     private final int bajoPeso = 0;
     private final int sobrePeso = 1;
+    private String informacion;
+    private String letras;
+    private int residuo;
+    private int calculoDNI;
 
     //    Constructores
-    public Persona(String dni) {
+    public Persona() {
         nombre = "";
         edad = "";
-        this.dni = dni;
         sexo = sexoDefecto;
         peso = 0;
         altura = 0;
     }
 
-    public void persona(String nombre, String edad, char sexo, String dni){
+    //    Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    //    Metodos
+    public void persona(String nombre, String edad, char sexo) {
         this.nombre = nombre;
         this.edad = edad;
-        this.dni = dni;
         this.sexo = sexo;
         peso = 0;
         altura = 0;
     }
 
-    public void persona(String nombre, String edad, char sexo, String dni, double peso, double altura) {
+    public void persona(String nombre, String edad, char sexo, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
-        this.dni = dni;
         this.sexo = sexo;
         this.peso = peso;
         this.altura = altura;
     }
 
-    public int calcularIMC(){
-        imc = peso/Math.pow(altura,2);
-        if (imc<20){
-           return pesoIdeal;
-        }else if (20<= imc && imc <= 25){
+    public int calcularIMC() {
+        imc = peso / Math.pow(altura, 2);
+        if (imc < 20) {
+            return pesoIdeal;
+        } else if (20 <= imc && imc <= 25) {
             return bajoPeso;
         }
         return sobrePeso;
     }
 
-    public boolean esMayorDeEdad(){
-        if (Integer.parseInt(edad)>=18){
+    public boolean esMayorDeEdad() {
+        if (Integer.parseInt(edad) >= 18) {
             return true;
         }
         return false;
     }
 
-    public void comprobarSexo(char sexo){
-        switch (sexo){
+    public void comprobarSexo(char sexo) {
+        switch (sexo) {
             case 'H':
                 break;
             case 'M':
@@ -71,9 +94,23 @@ public class Persona {
         }
     }
 
-//    public String toString(){
-//
-//    }
+    public String toString() {
+        informacion = "Nombre: " + nombre + "\n" +
+                "Edad: " + edad + "\n" +
+                "DNI: " + dni + "\n" +
+                "Sexo: " + sexo + "\n" +
+                "Peso: " + peso + "\n" +
+                "Altura: " + altura + "\n" +
+                "imc: " + imc;
+        return informacion;
+    }
 
+    public String generaDNI() {
+        calculoDNI = (int) Math.round(Math.random() * 90000000);
+        letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        residuo = calculoDNI % 23;
+        dni = String.valueOf(calculoDNI) + letras.charAt(residuo);
+        return dni;
+    }
 
 }
