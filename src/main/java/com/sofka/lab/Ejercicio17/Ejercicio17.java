837d355734f0b704bb462552fc50eb69ebe67e93
Aura Russill <auracris1996@hotmail.com>
2022-04-20 22:59:42 -0500

    Inicio de la clase ejecutable

--- src/main/java/com/sofka/lab/Ejercicio17/Ejercicio17.java
+++ src/main/java/com/sofka/lab/Ejercicio17/Ejercicio17.java
@@ -3,5 +3,21 @@ package com.sofka.lab.Ejercicio17;
 public class Ejercicio17 {
     public static void main(String[] args) {
 
+        Electrodomesticos [] listaElectrodomesticos = new Electrodomesticos [10];
+
+        listaElectrodomesticos [0] = new Lavadora();
+        listaElectrodomesticos [4] = new Lavadora(2);
+        listaElectrodomesticos [2] = new Lavadora(3);
+        listaElectrodomesticos [1] = new Lavadora(100, 1);
+        listaElectrodomesticos [3] = new Lavadora(200, 2);
+        listaElectrodomesticos [5] = new Television();
+        listaElectrodomesticos [6] = new Television(100, 1);
+        listaElectrodomesticos [7] = new Television(200, 2);
+        listaElectrodomesticos [8] = new Television(40, false);
+        listaElectrodomesticos [9] = new Television(40, true);
+
+        for (int i = 0; i < listaElectrodomesticos.length; i++){
+            listaElectrodomesticos[i].precioFinal();
+        }
     }
 }
