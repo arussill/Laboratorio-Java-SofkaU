e4000e38f1d9ce8bbbd7a2ce71fb3bd06d26f204
Aura Russill <auracris1996@hotmail.com>
2022-04-20 21:59:45 -0500

    Clase Lavadora creada eje17

--- src/main/java/com/sofka/lab/Ejercicio17/Electrodomesticos.java
+++ src/main/java/com/sofka/lab/Ejercicio17/Electrodomesticos.java
@@ -12,36 +12,36 @@ public class Electrodomesticos {
     protected final String colorDefecto = "blanco";
     protected final char consumoEnergeticoDefecto = 'F';
     protected final double pesoDefecto = 5;
-    protected char [] letras  = {'A', 'B', 'C', 'D', 'E', 'F'};
-    protected String colores [] = {"blanco", "negro", "rojo", "azul", "gris"};
+    protected char[] letras = {'A', 'B', 'C', 'D', 'E', 'F'};
+    protected String colores[] = {"blanco", "negro", "rojo", "azul", "gris"};
     protected int aumento;
 
 //    Constructores
 
-    public Electrodomesticos (){
+    public Electrodomesticos() {
         this.precioBase = precioBaseDefecto;
         this.color = colorDefecto;
         this.consumoEnergetico = consumoEnergeticoDefecto;
         this.peso = pesoDefecto;
-        comprobarConsumoEnergetico(this.consumoEnergetico );
+        comprobarConsumoEnergetico(this.consumoEnergetico);
         comprobarColor(this.color);
     }
 
-    public Electrodomesticos (int precioBase, double peso){
+    public Electrodomesticos(int precioBase, double peso) {
         this.precioBase = precioBase;
         this.color = colorDefecto;
         this.consumoEnergetico = consumoEnergeticoDefecto;
         this.peso = peso;
-        comprobarConsumoEnergetico(this.consumoEnergetico );
+        comprobarConsumoEnergetico(this.consumoEnergetico);
         comprobarColor(this.color);
     }
 
-    public Electrodomesticos (int precioBase, String color, char consumoEnergetico, double peso){
+    public Electrodomesticos(int precioBase, String color, char consumoEnergetico, double peso) {
         this.precioBase = precioBase;
         this.color = color;
         this.consumoEnergetico = consumoEnergetico;
         this.peso = peso;
-        comprobarConsumoEnergetico(this.consumoEnergetico );
+        comprobarConsumoEnergetico(this.consumoEnergetico);
         comprobarColor(this.color);
     }
 
@@ -65,64 +65,60 @@ public class Electrodomesticos {
 
 //    Metodos
 
-    private char comprobarConsumoEnergetico (char letra){
-        for (char elemento: letras) {
-            if (elemento!=letra){
+    private char comprobarConsumoEnergetico(char letra) {
+        for (char elemento : letras) {
+            if (elemento != letra) {
                 consumoEnergetico = consumoEnergeticoDefecto;
             }
         }
         return consumoEnergetico;
     }
 
-    private String comprobarColor(String color){
-        for (String elemento: colores){
-            if (elemento.equalsIgnoreCase(color)){
+    private String comprobarColor(String color) {
+        for (String elemento : colores) {
+            if (elemento.equalsIgnoreCase(color)) {
                 this.color = colorDefecto;
             }
         }
         return this.color;
     }
 
-    public int precioFinal(){
-        switch (consumoEnergetico){
+    public int precioFinal() {
+        switch (consumoEnergetico) {
             case 'A':
                 aumento = 100;
                 aumentoPorTamaño();
                 break;
             case 'B':
-                aumento = 80;
-                aumentoPorTamaño();
+                aumento = 80 + aumentoPorTamaño();
                 break;
             case 'C':
-                aumento = 60;
-                aumentoPorTamaño();
+                aumento = 60 + aumentoPorTamaño();
                 break;
             case 'D':
-                aumento = 50;
-                aumentoPorTamaño();
+                aumento = 50 + aumentoPorTamaño();
                 break;
             case 'E':
-                aumento = 30;
-                aumentoPorTamaño();
+                aumento = 30 + aumentoPorTamaño();
                 break;
             case 'F':
-                aumento = 10;
-                aumentoPorTamaño();
+                aumento = 10 + aumentoPorTamaño();
                 break;
         }
-       return (aumento+precioBase);
+        return (aumento + precioBase);
     }
 
-    private void aumentoPorTamaño() {
-        if ((0 <= peso) && (peso >= 19)){
+    protected int aumentoPorTamaño() {
+        if ((0 <= peso) && (peso >= 19)) {
             aumento += 10;
-        }else if ((20 <= peso) && (peso >= 49)){
+        } else if ((20 <= peso) && (peso >= 49)) {
             aumento += 50;
-        }else if ((50 <= peso) && (peso >= 79)){
+        } else if ((50 <= peso) && (peso >= 79)) {
             aumento += 80;
-        }else if((peso >= 80)){
+        } else if ((peso >= 80)) {
             aumento += 100;
         }
+        return aumento;
     }
 
 }
---
+++ src/main/java/com/sofka/lab/Ejercicio17/Lavadora.java
@@ -0,0 +1,46 @@
+package com.sofka.lab.Ejercicio17;
+
+//SubClase de Electrodomesticos
+public class Lavadora extends Electrodomesticos {
+
+//    Atributos
+
+    private double carga;
+    private final double cargaDefecto = 5;
+
+//    Constructores
+
+    public Lavadora() {
+        super();
+        this.carga = cargaDefecto;
+    }
+
+    public Lavadora(int precioBase, double peso) {
+        super(precioBase, peso);
+        super.precioBase = precioBase;
+        super.peso = peso;
+        this.carga = cargaDefecto;
+    }
+
+    public Lavadora(double carga) {
+        super();
+        this.carga = carga;
+    }
+
+//    Getters
+
+    public double getCarga() {
+        return carga;
+    }
+
+//    Metodos
+
+    @Override
+    public int precioFinal() {
+        if (carga >= 30){
+            aumento = 50 + super.precioFinal();
+        }
+        return aumento;
+
+    }
+}
